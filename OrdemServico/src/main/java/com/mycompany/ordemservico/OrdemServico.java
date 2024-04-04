76c45f17c3edf0de3faafc9f9af3485dce68d6ea
Renan Augusto <114965499+RenanAUG@users.noreply.github.com>
2024-04-03 22:07:03 -0300

    Adicionando projeto de Ordem e Serviço.

---
+++ OrdemServico/src/main/java/com/mycompany/ordemservico/OrdemServico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ordemservico;
+
+/**
+ *
+ * @author aluno
+ */
+public class OrdemServico {
+
+    public static void main(String[] args) {
+        new FrameLogin().setVisible(true);
+    }
+}
